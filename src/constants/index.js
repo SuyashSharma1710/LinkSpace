fba9efab66c04842603c029b13a272762d1ea437
SuyashSharma1710 <110248158+SuyashSharma1710@users.noreply.github.com>
2024-06-16 15:10:22 +0530

    added deploy workflow file

--- src/constants/index.js
+++ src/constants/index.js
@@ -168,7 +168,7 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      "I thought it was impossible to make a website as beautiful as our product, but Suyash proved me wrong.",
     name: "Sara Lee",
     designation: "CFO",
     company: "Acme Co",
@@ -176,7 +176,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      "I've never met a web developer who truly cares about their clients' success like Suyash does.",
     name: "Chris Brown",
     designation: "COO",
     company: "DEF Corp",
@@ -184,7 +184,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      "After Suyash optimized our website, our traffic increased by 50%. We can't thank them enough!",
     name: "Lisa Wang",
     designation: "CTO",
     company: "456 Enterprises",
