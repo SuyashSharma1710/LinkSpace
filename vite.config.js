97cbceeaaa46f3b063d9fe8e2fdc63251c4eb58d
SuyashSharma1710 <110248158+SuyashSharma1710@users.noreply.github.com>
2024-06-16 15:46:37 +0530

    Update Vite config and GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Suyash-Sharma-Portfolio/",
+  base: '/Suyash-Sharma-Portfolio/',
   build: {
     outDir: 'dist'
   }
